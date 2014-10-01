af1b53db6eb434f631f742f1916e0bb111579176
shrukul <shrukul99@gmail.com>
2014-10-01 11:18:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+cyanogenmod CM-11.0 device Repository for Galaxy Grand Quattro GT-I8552 (delos3geur)
+
+kitkat 4.4
+
+Work in progress
---
+++ delos3geur.mk
@@ -0,0 +1,149 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+DEVICE_PACKAGE_OVERLAYS += device/samsung/delos3geur/overlay
+
+## Video
+PRODUCT_PACKAGES += \
+    libstagefrighthw \
+    libmm-omxcore \
+    libOmxCore
+
+## Graphics
+PRODUCT_PACKAGES += \
+    copybit.msm7x27a \
+    gralloc.msm7x27a \
+    hwcomposer.msm7x27a \
+    libtilerenderer
+
+## Misc.
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs \
+    com.android.future.usb.accessory
+
+## Audio
+PRODUCT_PACKAGES += \
+    audio.primary.msm7x27a \
+    audio_policy.msm7x27a \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio_policy.conf \
+    libaudioutils
+
+## Other HALs
+PRODUCT_PACKAGES += \
+    camera.msm7x27a \
+    lights.msm7x27a \
+    gps.msm7x27a \
+    power.msm7x27a \
+    healthd.msm7x27a
+
+## FM radio
+PRODUCT_PACKAGES += \
+    qcom.fmradio \
+    libqcomfm_jni \
+    FM2
+
+## Recovery
+PRODUCT_COPY_FILES += \
+    device/samsung/delos3geur/recovery/sbin/rmt_storage_recovery:recovery/root/sbin/rmt_storage_recovery 
+
+## Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+
+## Ramdisk
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/samsung/delos3geur/ramdisk,root)
+
+## Prebuilt
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/samsung/delos3geur/prebuilt/system,system)
+
+PRODUCT_COPY_FILES += \
+    device/samsung/delos3geur/prebuilt/system/lib/liboncrpc.so:obj/lib/liboncrpc.so \
+    device/samsung/delos3geur/prebuilt/system/lib/libnv.so:obj/lib/libnv.so
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bluetooth.remote.autoconnect=true \
+    ro.bluetooth.request.master=true \
+    ro.bt.bdaddr_path=/data/misc/bluedroid/bdaddr \
+    ro.qualcomm.bluetooth.dun=true \
+    ro.qualcomm.bluetooth.ftp=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.gr.numframebuffers=3 \
+    debug.egl.recordable.rgba8888=1 \
+    debug.composition.type=dyn \
+    debug.hwc.dynThreshold=1.9 \
+    ro.bq.gpu_to_cpu_unsupported=1 \
+    ro.max.fling_velocity=4000 \
+    ro.opengles.version=131072 \
+    ro.sf.lcd_density=240
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dexopt-data-only=1 \
+    dalvik.vm.jit.codecachesize=1 \
+    ro.config.low_ram=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    lpa.decode=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    com.qc.hardware=true \
+    dev.pm.dyn_sample_period=700000 \
+    dev.pm.dyn_samplingrate=1 \
+    ro.vendor.extension_library=/system/lib/libqc-opt.so
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.vold.umsdirtyratio=50 
+
+#PRODUCT_PROPERTY_OVERRIDES += \
+# persist.webview.provider=classic
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.cwm.enable_key_repeat=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fm.analogpath.supported=true \
+    ro.fm.transmitter=false \
+    ro.fm.mulinst.recording.support=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=60
+
+## Other
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_TAGS += dalvik.gc.type-precise
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+$(call inherit-product, build/target/product/full.mk)
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
